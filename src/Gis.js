35dbd5af94429484c692a0ed6e9fd55244c9ea32
james-gould <jamesgould1994@gmail.com>
2017-04-11 17:09:41 +0100

    Structural changes.

---
+++ src/Gis.js
@@ -0,0 +1,13 @@
+/** 
+ * 
+ *  Gis.js is released under the MIT license, which should be retained when including, utilising or developing Gis.js in any environment.
+ * 
+ *  Original Creator: James Gould © 2017
+ * 
+ */
+
+(function Gis() {
+    
+    
+
+})();
