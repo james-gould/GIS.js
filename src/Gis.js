f8202e710abe1adf9c8329162e176965ded79c34
james-gould <jamesgould1994@gmail.com>
2017-04-13 13:28:07 +0100

    Initial functions and structure

--- src/Gis.js
+++ src/Gis.js
@@ -1,4 +1,4 @@
-/** 
+/* 
  * 
  *  Gis.js is released under the MIT license, which should be retained when including, utilising or developing Gis.js in any environment.
  * 
@@ -6,8 +6,38 @@
  * 
  */
 
-(function Gis() {
-    
-    
 
-})();
+/**
+ * Utility functions for working with GIS data. Do not export, use internally.
+ */
+var Utils = { 
+
+    /**
+     * Checks if the input is of a primitive value with no fractional parts.
+     */
+    isFloat: (value) => {
+        return value === +n && n !== (n|0);
+    }
+
+}
+
+
+/**
+ * Contains utility functions for working with GIS data.
+ * 
+ */
+var Gis = { 
+
+    ValidLatLng: (latitude, longitude, callback) => {
+        if(!Utils.isFloat(latitude) || !Utils.isFloat(longitude)) {
+            var err = new Error("Latitude/longitude pair is of an invalid datatype");
+            return callback(err)
+        }
+    },
+
+}
+
+// /**
+//  * Export only Gis, do not release any local objects or functions (extracted or otherwise) used.
+//  */
+// module.exports = Gis;
